ace79cf736c32efa5419448cbcf790a010fe84a6
bibash21-creator <bibashworkmail21@gmail.com>
2025-07-22 21:08:52 +0545

    Added first file

---
+++ roboknot/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        {/* For Landing Page */}
+        <Route path='/' 
+          element=''/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
